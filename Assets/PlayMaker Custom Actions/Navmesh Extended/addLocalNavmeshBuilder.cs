9702bf633b9613b6c29a8532b4ed5ad093d69677
Jean Fabre <contact@fabrejean.net>
2017-09-20 16:33:35 +0300

    added extended navmesh actions from Eric

---
+++ Assets/PlayMaker Custom Actions/Navmesh Extended/addLocalNavmeshBuilder.cs
@@ -0,0 +1,69 @@
+﻿// (c) Copyright HutongGames, LLC 2010-2017. All rights reserved.
+// License: Attribution 4.0 International (CC BY 4.0)
+/*--- __ECO__ __PLAYMAKER__ __ACTION__ ---*/
+
+using UnityEngine;
+
+namespace HutongGames.PlayMaker.Actions
+{
+	[ActionCategory("Navmesh Extended")]
+	[Tooltip("Add a local navmesh builder component to a gameobject for local realtime navmesh updates.")]
+	public class addLocalNavmeshBuilder : FsmStateAction
+
+	{
+		[RequiredField]
+		[Tooltip("The GameObject to add the Navmesh Builder to.")]
+		[Title("Gameobject")]  
+		public FsmOwnerDefault gameObject;
+		
+		[ActionSection("Optional")]
+		
+		[Title("Builder Size")]  
+		[Tooltip("The size of the build bounds in vector3.")]
+		public FsmVector3 size;
+		
+		[Title("Tracked Gameobject")]  
+		[Tooltip("Set the center of the builder.")]
+		public FsmGameObject trackedObject;
+		
+
+		public override void Reset()
+		{
+			
+			gameObject = null;
+			size = new FsmVector3 (){UseVariable=true};
+			trackedObject = new FsmGameObject (){UseVariable=true};
+		}
+
+		public override void OnEnter()
+		{
+			var go = Fsm.GetOwnerDefaultTarget(gameObject);
+
+			doBuilder();
+			Finish();
+
+		}
+
+		void doBuilder()
+		{
+			var go = Fsm.GetOwnerDefaultTarget(gameObject);
+			if (go == null)
+			{
+				return;
+			}
+			
+			LocalNavMeshBuilder mesh = go.AddComponent(typeof(LocalNavMeshBuilder)) as LocalNavMeshBuilder;
+			
+			if (!size.IsNone)
+			{
+				mesh.m_Size = size.Value;
+			}
+			
+			if (!trackedObject.IsNone)
+			{
+				mesh.m_Tracked = trackedObject.Value.transform;
+			}
+			
+		}
+	}
+}
---
+++ Assets/PlayMaker Custom Actions/Navmesh Extended/addNavmeshSourceTag.cs
@@ -0,0 +1,48 @@
+﻿// (c) Copyright HutongGames, LLC 2010-2017. All rights reserved.
+// License: Attribution 4.0 International (CC BY 4.0)
+/*--- __ECO__ __PLAYMAKER__ __ACTION__ ---*/
+
+using UnityEngine;
+
+namespace HutongGames.PlayMaker.Actions
+{
+	[ActionCategory("Navmesh Extended")]
+	[Tooltip("Add a nav mesh source tag component to game object to be recognized for realtime nav mesh updates.")]
+
+	public class addNavmeshSourceTag : FsmStateAction
+
+	{
+		[RequiredField]
+		[Tooltip("The GameObject to add the Navmesh Source Tag to.")]
+		[Title("Gameobject")]  
+		public FsmOwnerDefault gameObject;
+
+		public override void Reset()
+		{
+			
+			gameObject = null;
+
+		}
+
+		public override void OnEnter()
+		{
+			var go = Fsm.GetOwnerDefaultTarget(gameObject);
+
+			addTag();
+			Finish();
+
+		}
+
+		void addTag()
+		{
+			var go = Fsm.GetOwnerDefaultTarget(gameObject);
+			if (go == null)
+			{
+				return;
+			}
+			
+			NavMeshSourceTag mesh = go.AddComponent(typeof(NavMeshSourceTag)) as NavMeshSourceTag;
+
+		}
+	}
+}
